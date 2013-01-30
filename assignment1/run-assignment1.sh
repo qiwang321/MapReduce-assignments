384922820219cdc0384ce9daffe4a89f9d9e247c
insightlight <qiwang321@gmail.com>
2013-01-30 05:35:36 -0500

    assignment1

--- assignment1/run-assignment1.sh
+++ assignment1/run-assignment1.sh
@@ -1,2 +1,3 @@
-DIR=$( dirname "$BASH_SOURCE[0]")
+#!/bin/sh
+DIR=$(dirname $0)
 hadoop jar $DIR/dist/cloud9-1.4.6-1.jar edu.umd.cloud9.example.simple.DemoWordCount -libjars $DIR/dist/cloud9-1.4.6-1.jar -input bible+shakes.nopunc.gz -output qiwang321 -numReducers 5
